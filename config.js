dcc3a9bad58caee4fc91e50a00e8c61fc474367f
孙一飞 <sunyifei84@gmail.com>
2018-10-17 14:25:20 +0800

    Delete config.js
    remove uncorrect config

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  appid: 'appId_of_your_app',
-  port: 8131,
-  secret: 'secret_of_your_app',
-  enable_mongoose_debug: false,
-  log_level: process.env.LOG_LEVEL || 'debug',
-  mongo_url: process.env.MONGODB || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/dbname',
-};
